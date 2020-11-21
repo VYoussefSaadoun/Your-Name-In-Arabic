14d1c9302f0e9d3e21e2a25e27af470e8a3ce859
Youssef Saadoun <youssefsaadoun55@gmail.com>
2020-11-21 17:57:15 +0200

    Added Infinty loop and Added some comments

--- YNIA.py
+++ YNIA.py
@@ -1,4 +1,5 @@
 
+# Array For Replacing to Arabic letters
 arabic = ["ا", "ب" , "س", "د", "ي", "ف", "ج", "ه", "ي", "چ", "ك", "ل", "م", "ن", "و", "ب", "ك", "ر", "س", "ت", "يو", "ف", "و", "ك", "ي", "ز"]
 
 
@@ -8,6 +9,12 @@ def eTA(name):
     count = -1
     arr = []
     mname = name
+
+    #To Stop The Game
+    if name == "exit" or name == "Exit":
+        return -1
+
+    #loop to remove repeatitive letters that may cause problem in arabic
     for letter in name:
         count += 1
         if count == 0:
@@ -16,8 +23,12 @@ def eTA(name):
             mname = name.replace(name[count], "", 1)
         else:
             arr.append(letter)
+    
+    #Replacing English letters by Arabic letters
     for letter in mname:
         counter += 1
+        
+        #from line 32 to 94 for special pronunciation of english letters
         if letter in "ACcEeIHhSsuXxTt":
             if  letter == "A":
                 in_Arabic += "أ"
@@ -81,16 +92,31 @@ def eTA(name):
                     in_Arabic += "ك"
             elif letter in "ZzX":
                 in_Arabic += "ز"
-        elif ord(letter) == 32:
-            in_Arabic += " "
+        
+        #For replacing letter by letter depending on the Ascci number & the Arabic array.
         elif ord(letter) in range(65, 90):
             letter_index = ord(letter) - 65
             in_Arabic += arabic[letter_index]
-        else:
+        elif ord(letter) in range(97, 122):
             letter_index = ord(letter) - 97
             in_Arabic += arabic[letter_index]
         
+        #For any sympols or digits other than the normal letters
+        else:
+            in_Arabic += letter
             
     return in_Arabic
-result = eTA(input("Enter Your Name: "))
-print("Your Name In Arabic is: ", result)
+
+#Infinity loop to keep running the game
+while True:
+    print("Please Enter your name in english letters (Type exit to stop the game)",
+    "\n **************************************")
+
+    #Loop breaked if (exit) was typed
+    result = eTA(input("Enter Your Name: "))
+    if result == -1 :
+        print("Thanks For playing with me ^_^")
+        break
+
+    else:
+        print("Your Name In Arabic is: ", result, "\n ###################################### \n")
